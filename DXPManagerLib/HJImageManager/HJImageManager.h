3a209db4d84a6075e16d07f2fe5a991f36980acd
李标 <li.biao3@iwhalecloud.com>
2024-12-24 16:41:13 +0800

    fix

--- DXPManagerLib/HJImageManager/HJImageManager.h
+++ DXPManagerLib/HJImageManager/HJImageManager.h
@@ -27,9 +27,6 @@ NS_ASSUME_NONNULL_BEGIN
 // 获取svg 图片
 - (UIImage *)getSVGImageByName:(NSString *)name tintColor:(NSString *)colorStr;
 
-// 获取svg 图片
-- (UIImage *)getSVGImageByName:(NSString *)name tintColor:(NSString *)colorStr;
-
 @end
 
 NS_ASSUME_NONNULL_END
