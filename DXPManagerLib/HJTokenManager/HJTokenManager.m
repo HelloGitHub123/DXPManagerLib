ee42da4ac31770243bb6c76e0eaa95ebc763360c
李标 <li.biao3@iwhalecloud.com>
2026-08-04 16:12:41 +0800

    fix

--- DXPManagerLib/HJTokenManager/HJTokenManager.m
+++ DXPManagerLib/HJTokenManager/HJTokenManager.m
@@ -12,9 +12,9 @@
 #import "ManagerHeader.h"
 #import <objc/runtime.h>
 
-#define M_PI        3.14159265358979323846264338327950288  /* pi            */
+#define M_PI 3.14159265358979323846264338327950288  /* pi            */
 
-#define M_PI_2      1.57079632679489661923132169163975144  /* pi/2          */
+#define M_PI_2 1.57079632679489661923132169163975144  /* pi/2          */
 
 static HJTokenManager *tokenManager = nil;
 
